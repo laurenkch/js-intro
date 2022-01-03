75580235a6bd119cb6d2109fac84f074e2fa3c4f
laurenkch <lkoch879@gmail.com>
2022-01-03 17:10:58 -0500

    rest of strings assignment

--- strings.js
+++ strings.js
@@ -93,7 +93,22 @@
     function strings4() {
         const section = document.createElement('section');
         body.appendChild(section);
+        const theorem = 'Pythagorean theorem';
+        const a = 5;
+        const b = 8;
 
+        const myString = `Using ${theorem}, we can work out that that if the two shortest sides of a right-angled triangle have lengths of ${a} and ${b}, the length of the hypotenuse is ${ Math.sqrt((a*a) +(b*b))}.`;
+
+
+
+        // Don't edit the code below here!
+
+        section.innerHTML = ' ';
+        const para1 = document.createElement('p');
+        para1.textContent = myString;
+
+        section.appendChild(para1);
+    
     }
 
     strings4();
